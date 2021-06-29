d91e1cf4f7fc9193506b6919e13f02fd729641f7
Rockzy <74587590+rockzy77@users.noreply.github.com>
2021-06-29 14:24:37 +0530

    Add files via upload

---
+++ btw.py
@@ -0,0 +1,375 @@
+import os
+
+def main():
+    os.system('clear')
+    print("")
+    print("\033[36m                              ||")
+    print("\033[36m                            ||||||")
+    print("\033[36m                          ||||||||||")
+    print("\033[36m                        |||||||||||||")
+    print("\033[36m                      |||||||||||||||||")
+    print("\033[36m                   |||||||||||||||||||||||")
+    print("\033[36m                ||||||||||||||||||||||||||||")
+    print("\033[36m             ||||||||||||||||||||||||||||||||||")
+    print("")
+    print("""\033[36m
+        ____  _     ______    _       __               __
+    / __ )(_)___/_  __/___| |     / /___  _________/ /
+    / __  / / __ \/ / / __ \ | /| / / __ \/ ___/ __  / 
+    / /_/ / / / / / / / /_/ / |/ |/ / /_/ / /  / /_/ /  
+    /_____/_/_/ /_/_/  \____/|__/|__/\____/_/   \__,_/  """)
+    print("")
+
+
+def convert(n):
+    if(n == 32):
+        n = " "
+        arr.append(n)    
+    if(n == 33):
+        n = "!"
+        arr.append(n)
+    if(n == 34):
+        n = "\""
+        arr.append(n)
+    if(n == 35):
+        n = "#"
+        arr.append(n)
+    if(n == 36):
+        n = "$"
+        arr.append(n)  
+    if(n == 37):
+        n = "%"
+        arr.append(n)
+    if(n == 38):
+        n = "&"
+        arr.append(n)
+    if(n == 39):
+        n = "'"
+        arr.append(n)
+    if(n == 40):
+        n = "("
+        arr.append(n)
+    if(n == 41):
+        n = ")"
+        arr.append(n)
+    if(n == 42):
+        n = "*"
+        arr.append(n)
+    if(n == 43):
+        n = "+"
+        arr.append(n)
+    if(n == 44):
+        n = ","
+        arr.append(n)
+    if(n == 45):
+        n = "-"
+        arr.append(n)
+    if(n == 46):
+        n = "."
+        arr.append(n)
+    if(n == 47):
+        n = "/"
+        arr.append(n)
+    if(n == 48):
+        n = "0"
+        arr.append(n)
+    if(n == 49):
+        n = "1"
+        arr.append(n) 
+    if(n == 50):
+        n = "2"
+        arr.append(n)
+    if(n == 51):
+        n = "3"
+        arr.append(n)
+    if(n == 52):
+        n = "4"
+        arr.append(n)
+    if(n == 53):
+        n = "5"
+        arr.append(n)
+    if(n == 54):
+        n = "6"
+        arr.append(n)
+    if(n == 55):
+        n = "7"
+        arr.append(n)
+    if(n == 56):
+        n = "8"
+        arr.append(n)
+    if(n == 57):
+        n = "9"
+        arr.append(n)
+    if(n == 58):
+        n = ":"
+        arr.append(n)
+    if(n == 59):
+        n = ";"
+        arr.append(n)   
+    if(n == 60):
+        n = "<"
+        arr.append(n)
+    if(n == 61):
+        n = "="
+        arr.append(n)
+    if(n == 62):
+        n = ">"
+        arr.append(n)
+    if(n == 63):
+        n = "?"
+        arr.append(n)
+    if(n == 64):
+        n = "@"
+        arr.append(n)
+    if(n == 65):
+        n = "A"
+        arr.append(n) 
+    if(n == 66):
+        n = "B"
+        arr.append(n)
+    if(n == 67):
+        n = "C"
+        arr.append(n)
+    if(n == 68):
+        n = "D"
+        arr.append(n)
+    if(n == 69):
+        n = "E"
+        arr.append(n)  
+    if(n == 70):
+        n = "F"
+        arr.append(n)
+    if(n == 71):
+        n = "G"
+        arr.append(n) 
+    if(n == 72):
+        n = "H"
+        arr.append(n) 
+    if(n == 73):
+        n = "I"
+        arr.append(n) 
+    if(n == 74):
+        n = "J"
+        arr.append(n) 
+    if(n == 75):
+        n = "K"
+        arr.append(n) 
+    if(n == 76):
+        n = "L"
+        arr.append(n) 
+    if(n == 77):
+        n = "M"
+        arr.append(n) 
+    if(n == 78):
+        n = "N"
+        arr.append(n) 
+    if(n == 79):
+        n = "O"
+        arr.append(n) 
+    if(n == 80):
+        n = "P"
+        arr.append(n) 
+    if(n == 81):
+        n = "Q"
+        arr.append(n)   
+    if(n == 82):
+        n = "R"
+        arr.append(n) 
+    if(n == 83):
+        n = "S"
+        arr.append(n) 
+    if(n == 84):
+        n = "T"
+        arr.append(n) 
+    if(n == 85):
+        n = "U"
+        arr.append(n) 
+    if(n == 86):
+        n = "V"
+        arr.append(n) 
+    if(n == 87):
+        n = "W"
+        arr.append(n) 
+    if(n == 88):
+        n = "X"
+        arr.append(n) 
+    if(n == 89):
+        n = "Y"
+        arr.append(n) 
+    if(n == 90):
+        n = "Z"
+        arr.append(n)
+    if(n == 91):
+        n = "["
+        arr.append(n) 
+    if(n == 92):
+        n = "\\"
+        arr.append(n)
+    if(n == 93):
+        n = "]"
+        arr.append(n) 
+    if(n == 94):
+        n = "^"
+        arr.append(n) 
+    if(n == 95):
+        n = "_"
+        arr.append(n) 
+    if(n == 96):
+        n = "`"
+        arr.append(n) 
+    if(n == 97):
+        n = "a"
+        arr.append(n) 
+    if(n == 98):
+        n = "b"
+        arr.append(n) 
+    if(n == 99):
+        n = "c"
+        arr.append(n) 
+    if(n == 100):
+        n = "d"
+        arr.append(n) 
+    if(n == 101):
+        n = "e"
+        arr.append(n) 
+    if(n == 102):
+        n = "f"
+        arr.append(n) 
+    if(n == 103):
+        n = "g"
+        arr.append(n)                                                                                                                                                                                                                                                                                         
+    if(n == 104):
+        n = "h"
+        arr.append(n) 
+    if(n == 105):
+        n = "i"
+        arr.append(n) 
+    if(n == 106):
+        n = "j"
+        arr.append(n) 
+    if(n == 107):
+        n = "k"
+        arr.append(n) 
+    if(n == 108):
+        n = "l"
+        arr.append(n)
+    if(n == 109):
+        n = "m"
+        arr.append(n)
+    if(n == 110):
+        n = "n"
+        arr.append(n)
+    if(n == 111):
+        n = "o"
+        arr.append(n)
+    if(n == 112):
+        n = "p"
+        arr.append(n)
+    if(n == 113):
+        n = "q"
+        arr.append(n)
+    if(n == 114):
+        n = "r"
+        arr.append(n)
+    if(n == 115):
+        n = "s"
+        arr.append(n)
+    if(n == 116):
+        n = "t"
+        arr.append(n)
+    if(n == 117):
+        n = "u"
+        arr.append(n)
+    if(n == 118):
+        n = "v"
+        arr.append(n)
+    if(n == 119):
+        n = "w"
+        arr.append(n)
+    if(n == 120):
+        n = "x"
+        arr.append(n)
+    if(n == 121):
+        n = "y"
+        arr.append(n)
+    if(n == 122):
+        n = "z"
+        arr.append(n)
+    if(n == 123):
+        n = "{"
+        arr.append(n)                                                             
+    if(n == 124):
+        n = "|"
+        arr.append(n)    
+    if(n == 125):
+        n = "}"
+        arr.append(n)
+    if(n == 126):
+        n = "~"
+        arr.append(n)                       
+    else:
+        n = ""
+        arr.append(n)  
+
+main()
+
+print("\033[36m [0] Type binary text")
+print("\033[36m [1] Select a binary text file")
+print("")
+method = input("\033[36m Select a method: ")
+
+if(method == "1"):
+    arr = []
+    bin_input = input("\033[36m Enter binary text file path: ")
+    b = open(bin_input)
+    str = b.read()
+    words = str.split()
+    for word in words:
+        wl = len(word)
+        f = open("converted.txt", "w+")
+        n=0
+        for i in range(wl):
+            n= n+int(word[::-1][i]) * 2**i
+        convert(n)                      
+    f.writelines(arr)
+    l =""
+    for i in arr:
+        l = l+i         
+    print("")    
+    print("\033[33m Covertion is Successfull") 
+    print("\033[33m -----------------------------")   
+    print("\033[32m Converted Text: \n \""+l+"\"")
+    print("\033[33m -------------------------------")        
+    print("\033[33m Text saved to \"converted.txt\"")   
+    print("\033[33m -------------------------------")      
+            
+            
+
+
+elif(method == "0"):
+    bin_input = input(" Enter your binary text: ")
+    words = bin_input.split()
+    arr = []
+    for word in words:
+        wl = len(word)
+        f = open("converted.txt", "w+")
+        n=0
+        for i in range(wl):
+            n= n+int(word[::-1][i]) * 2**i
+        convert(n)                        
+    f.writelines(arr)
+    l =""
+    for i in arr:
+        l = l+i         
+    print("")    
+    print("\033[33m Covertion is Successfull") 
+    print("\033[33m -----------------------------")   
+    print("\033[32m Converted Text: \n \""+l+"\"")
+    print("\033[33m -------------------------------")        
+    print("\033[33m Text saved to \"converted.txt\"")   
+    print("\033[33m -------------------------------")         
+
+
+
+else:
+    print("\033[31m Invalid method")
